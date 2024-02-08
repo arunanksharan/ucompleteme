0a789e0a4534a4cfc74c17680852efe3b811482d
arunanksharan <arunank.sharan@gmail.com>
2024-02-08 09:10:55 +0530

    bug: reinitilaised supabase table with id as uuid

--- app/api/home/route.ts
+++ app/api/home/route.ts
@@ -1,30 +1,59 @@
-import { homeImageUrl, openApiTextCompleteUrl } from '@/constants/urls';
+import {
+  errorImageUrl,
+  homeAsPostUrl,
+  homeImageUrl,
+  openApiTextCompleteUrl,
+} from '@/constants/urls';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { NextRequest, NextResponse } from 'next/server';
 
 export const dynamic = 'force-dynamic';
 
 export async function POST(req: NextRequest): Promise<NextResponse> {
-  return new NextResponse(
-    `<!DOCTYPE html>
-      <html>
-        <head>
-          <title>UCompleteMe</title>
-          <meta property="og:title" content="UCompleteMe" />
-          <meta property="og:image" content="${homeImageUrl}" />
-          <meta name="fc:frame" content="vNext" />
-          <meta name="fc:frame:image" content="${homeImageUrl}" />
-          <meta name="fc:frame:post_url" content="${openApiTextCompleteUrl}" />
-          <meta name="fc:frame:input:text" content="Enter Text Here" />
-          <meta name="fc:frame:button:1" content="Complete" />
-        </head>
-        <body><img src="/images/welcome.png" alt="welcome to ucomplteme"></img></body>
-      </html>`,
-    {
-      status: 200,
-      headers: {
-        'Content-Type': 'text/html',
-      },
-    }
-  );
+  try {
+    return new NextResponse(
+      `<!DOCTYPE html>
+          <html>
+            <head>
+              <title>UCompleteMe</title>
+              <meta property="og:title" content="UCompleteMe" />
+              <meta property="og:image" content="${homeImageUrl}" />
+              <meta name="fc:frame" content="vNext" />
+              <meta name="fc:frame:image" content="${homeImageUrl}" />
+              <meta name="fc:frame:post_url" content="${openApiTextCompleteUrl}" />
+              <meta name="fc:frame:input:text" content="Enter Text Here" />
+              <meta name="fc:frame:button:1" content="Complete" />
+            </head>
+            <body><img src="/images/welcome.png" alt="welcome to ucomplteme"></img></body>
+          </html>`,
+      {
+        status: 200,
+        headers: {
+          'Content-Type': 'text/html',
+        },
+      }
+    );
+  } catch (error) {
+    return new NextResponse(
+      `<!DOCTYPE html>
+<html>
+<head>
+  <title>UCompleteMe</title>
+  <meta property="og:title" content="UCompleteMe" />
+  <meta property="og:image" content="${errorImageUrl}" />
+  <meta name="fc:frame" content="vNext" />
+  <meta name="fc:frame:image" content="${errorImageUrl}" />
+  <meta name="fc:frame:post_url" content="${homeAsPostUrl}" />
+  <meta name="fc:frame:button:1" content="Home" />
+</head>
+<body>UCompleteMe</body>
+</html>`,
+      {
+        status: 200,
+        headers: {
+          'Content-Type': 'text/html',
+        },
+      }
+    );
+  }
 }
--- app/api/resImage/route.tsx
+++ app/api/resImage/route.tsx
@@ -16,16 +16,16 @@ export const dynamic = 'force-dynamic';
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
   // Read from database
-
-  const imageId = req.nextUrl.searchParams.get('imageId') as string;
-  const { data, error } = await supabase
-    .from('queries')
-    .select('*')
-    .eq('id', imageId);
-
-  if (error) {
-    return new NextResponse(
-      `<!DOCTYPE html>
+  try {
+    const imageId = req.nextUrl.searchParams.get('imageId') as string;
+    const { data, error } = await supabase
+      .from('queries')
+      .select('*')
+      .eq('id', imageId);
+    // .order('id', { ascending: false })
+    if (error) {
+      return new NextResponse(
+        `<!DOCTYPE html>
     <html>
       <head>
         <title>UCompleteMe</title>
@@ -38,6 +38,74 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
       </head>
       <body>UCompleteMe</body>
     </html>`,
+        {
+          status: 200,
+          headers: {
+            'Content-Type': 'text/html',
+          },
+        }
+      );
+    }
+
+    console.log('data', data);
+
+    const openAiRes = await data[0].openai_response;
+    console.log('openAiRes', openAiRes);
+    const svg = await satori(
+      <div
+        style={{
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          display: 'flex',
+          width: '100%',
+          height: '100%',
+          backgroundColor: '#f4f4f4',
+          padding: 50,
+          lineHeight: 1.2,
+          fontSize: 24,
+        }}
+      >
+        {openAiRes}
+      </div>,
+      {
+        width: 600,
+        height: 400,
+        fonts: [
+          {
+            data: fontData,
+            name: 'Roboto',
+            style: 'normal',
+            weight: 400,
+          },
+        ],
+      }
+    );
+
+    // Convert SVG to PNG using Sharp
+    const pngBuffer = await sharp(Buffer.from(svg)).toFormat('png').toBuffer();
+
+    return new NextResponse(pngBuffer, {
+      status: 200,
+      headers: {
+        'Content-Type': 'image/png',
+        'Cache-Control': 'max-age=10',
+      },
+    });
+  } catch (error) {
+    return new NextResponse(
+      `<!DOCTYPE html>
+<html>
+  <head>
+    <title>UCompleteMe</title>
+    <meta property="og:title" content="UCompleteMe" />
+    <meta property="og:image" content="${errorImageUrl}" />
+    <meta name="fc:frame" content="vNext" />
+    <meta name="fc:frame:image" content="${errorImageUrl}" />
+    <meta name="fc:frame:post_url" content="${homeAsPostUrl}" />
+    <meta name="fc:frame:button:1" content="Home" />
+  </head>
+  <body>UCompleteMe</body>
+</html>`,
       {
         status: 200,
         headers: {
@@ -46,46 +114,4 @@ export async function GET(req: NextRequest): Promise<NextResponse> {
       }
     );
   }
-
-  const openAiRes = data[0].openai_response;
-  const svg = await satori(
-    <div
-      style={{
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-        display: 'flex',
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'f4f4f4',
-        padding: 50,
-        lineHeight: 1.2,
-        fontSize: 24,
-      }}
-    >
-      {openAiRes}
-    </div>,
-    {
-      width: 600,
-      height: 400,
-      fonts: [
-        {
-          data: fontData,
-          name: 'Roboto',
-          style: 'normal',
-          weight: 400,
-        },
-      ],
-    }
-  );
-
-  // Convert SVG to PNG using Sharp
-  const pngBuffer = await sharp(Buffer.from(svg)).toFormat('png').toBuffer();
-
-  return new NextResponse(pngBuffer, {
-    status: 200,
-    headers: {
-      'Content-Type': 'image/png',
-      'Cache-Control': 'max-age=10',
-    },
-  });
 }
